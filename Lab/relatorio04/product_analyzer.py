9f18f123b8b21de4e3f0484f0b90c7aed97edd62
Felipe Ribeiro <125563054+FelipeRibeiro12@users.noreply.github.com>
2023-09-11 16:31:17 -0300

    final solution

--- Lab/relatorio04/product_analyzer.py
+++ Lab/relatorio04/product_analyzer.py
@@ -1,13 +1,45 @@
-from database import Database
-from product_analyzer import ProductAnalyzer
-from utils import writeAJson
 
-db = Database(database="loja_de_roupas", collection="vendas")
-db.resetDatabase()
 
-pa = ProductAnalyzer(db.collection)
+class ProductAnalyzer:
+    def __init__(self, collection):
+        self.collection = collection
 
-writeAJson(pa.totalFromB(), 'Total de gastos do cliente B')
-writeAJson(pa.leastSoldProduct(), 'Produto menos vendido')
-writeAJson(pa.lessSpender(), 'Cliente que menos gastou')
-writeAJson(pa.productsSoldWithTwoOrMoreUnits(), 'Produtos vendidos com mais de 2 unidades')
+    def totalFromB(self):
+        result = self.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$match": {"cliente_id": "B"}},
+            {"$group": {"_id": "$cliente_id",
+                        "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
+            {"$group": {"_id": 2, "total": {"$avg": "$total"}}}
+        ])
+        return result
+
+    def leastSoldProduct(self):
+        result = self.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {"_id": "$produtos.nome", "total": {
+                "$sum": "$produtos.quantidade"}}},
+            {"$sort": {"total": 1}},
+            {"$limit": 1}
+        ])
+        return result
+
+    def lessSpender(self):
+        result = self.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$group": {"_id": {"cliente": "$cliente_id", "data": "$data_compra"},
+                        "total": {"$sum": {"$multiply": ["$produtos.quantidade", "$produtos.preco"]}}}},
+            {"$sort": {"_id.data": 1, "total": 1}},
+            {"$group": {"_id": "$'_id.data'", "cliente": {"$first": "$_id.cliente"},
+                        "total": {"$first": "$total"}}}
+        ])
+        return result
+
+    def productsSoldWithTwoOrMoreUnits(self):
+        result = self.collection.aggregate([
+            {"$unwind": "$produtos"},
+            {"$match": {"produtos.quantidade": {"$gt": 2}}},
+            {"$group": {"_id": "$produtos.nome", "total": {
+                "$sum": "$produtos.quantidade"}}}
+        ])
+        return result
